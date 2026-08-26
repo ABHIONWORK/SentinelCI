7447eb167a55267c5f7c5fd66b136902e46b3ce2
Abhishek kumar <Abhishektoy12@gmail.com>
2026-08-26 15:22:23 +0530

    Final commit beofre improvement

--- runner.sh
+++ runner.sh
@@ -5,6 +5,18 @@ log_message() {
     # $1 inside a function means the first argument passed TO THE FUNCTION
     echo "[SentinelCI] $1"
 }
+
+# Function to send Discord notifications
+send_notification() {
+    local message=$1
+    local webhook_url="https://discord.com/api/webhooks/1542102780385173574/mfWdp_uVoIfLJtNKl8tQ716nFpE2DV58NtVuPvLjBouEpZo6L3s9YmX4WvFWzn-9Hsfj"
+    
+    # Build the JSON payload
+    local json_payload="{\"content\": \"$message\"}"
+    
+    # Use curl to send a POST request to Discord silently (-s)
+    curl -s -H "Content-Type: application/json" -d "$json_payload" "$webhook_url" > /dev/null
+}
 show_help() {
     echo "Usage: ./runner.sh [OPTIONS]"
     echo "Options:"
@@ -23,35 +35,38 @@ case "$1" in
     --help)
         show_help
         ;;
-    --run)
+        --run)
         log_message "Starting the pipeline..."
         log_message "Step 1: Compiling Java code..."
         
-        # Run the Java compiler and send all output/errors to a log file
+        # Compile the code
         javac target_app/Main.java > logs/build.log 2>&1
         
-        # Check the exit code of the javac command
+        # Check if the build passed or failed
         if [ $? -eq 0 ]; then
             log_message "✅ Build Successful!"
         else
             log_message "❌ Build Failed! Check logs/build.log for details."
-            exit 1 # Stop the pipeline immediately
+            send_notification "🚨 **SentinelCI Alert:** Build Failed! Check logs for syntax errors."
+            exit 1 # Stop the pipeline
         fi
-	# --- DAY 4 ADDITION STARTS HERE ---
+
         log_message "Step 2: Running automated tests..."
         
-        # Execute the compiled Java app and pass it some dummy arguments
-        # We use >> to APPEND to the log file instead of overwriting it
+        # Run the compiled code
         java -cp target_app Main "TestCase1" "TestCase2" >> logs/build.log 2>&1
         
+        # Check if the tests passed or crashed
         if [ $? -eq 0 ]; then
             log_message "✅ All Tests Passed (Execution Successful)!"
+            send_notification "✅ **SentinelCI Success:** Build and tests passed perfectly! Ready for deployment."
         else
             log_message "❌ Tests Failed! App crashed during runtime. Check logs/build.log."
-            exit 1
+            send_notification "🚨 **SentinelCI Alert:** Tests Failed during runtime! App crashed."
+            exit 1 # Stop the pipeline
         fi
-        # --- DAY 4 ADDITION ENDS HERE ---
         ;;
+        # --- DAY 4 ADDITION ENDS HERE ---
     *)
         log_message "Error: Unknown command '$1'"
         show_help
